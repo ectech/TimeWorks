16c30b6321d165eb1040fe77fc3551c9279c0282
Jamil Evans <jamil@evanschambers.com>
2011-09-04 09:09:27 -0400

    Initial commit

---
+++ test/unit/timeworks/FoursquareControllerTests.groovy
@@ -0,0 +1,17 @@
+package timeworks
+
+import grails.test.*
+
+class FoursquareControllerTests extends ControllerUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
